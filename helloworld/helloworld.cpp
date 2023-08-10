1dfb9c0336acc0d31053703431eeb5f571a8219a
EthanTwo2022 <87847176+EthanTwo2022@users.noreply.github.com>
2023-08-10 17:41:51 +0800

    添加项目文件。

---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,17 @@
+﻿// helloworld.cpp: 定义应用程序的入口点。
+//
+
+#include "helloworld.h"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	//定义一个学生对象
+	Student stu{ "001", "zhangsan", 18, "1995-09-08" };
+	//将学生信息再次输出到控制台
+	//这里可以直接输出stu对象
+	std::cout << "学生信息：" << stu << std::endl;
+
+	return 0;
+}
